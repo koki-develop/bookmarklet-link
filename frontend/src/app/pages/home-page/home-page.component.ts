bf434d0080e5a41e8229957b2bd93b7b7eeadcdb
koki-develop <kou.pg.0131@gmail.com>
2024-04-15 08:08:49 +0900

    Add bookmarklet feature to home page

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CodeEditorModule, CodeModel } from '@ngstack/code-editor';
+
+const initialCode = `
+(function () {
+  alert('Hello, world!');
+})();
+`.trim();
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CodeEditorModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
+  readonly #sanitizer = inject(DomSanitizer);
 
+  readonly code = signal<string>(initialCode);
+
+  readonly codeModel: CodeModel = {
+    language: 'javascript',
+    value: initialCode,
+    uri: 'index.js',
+  };
+
+  readonly editorTheme = 'vs-dark';
+
+  readonly editorOptions = {
+    contextmenu: false,
+    minimap: {
+      enabled: false,
+    },
+  };
+
+  readonly bookmarklet = computed(() => {
+    const code = this.code();
+    return `javascript:${encodeURIComponent(code)}`;
+  });
+
+  readonly sanitizedBookmarklet = computed(() => {
+    const bookmarklet = this.bookmarklet();
+    return this.#sanitizer.bypassSecurityTrustUrl(bookmarklet);
+  });
 }
