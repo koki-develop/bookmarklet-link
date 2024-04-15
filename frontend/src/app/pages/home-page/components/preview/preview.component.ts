ad0c735340d568c362b918902607510ec7989297
koki-develop <kou.pg.0131@gmail.com>
2024-04-15 21:51:30 +0900

    Add copy feedback and update imports

--- frontend/src/app/pages/home-page/components/preview/preview.component.ts
+++ frontend/src/app/pages/home-page/components/preview/preview.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, inject } from '@angular/core';
-import { NgIconComponent } from '@ng-icons/core';
+import { Component, input, inject, computed, signal } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import copy from 'copy-to-clipboard';
 import { BookmarkletPipe } from '../../../../pipes/bookmarklet.pipe';
 import { SafeUrlPipe } from '../../../../pipes/safe-url.pipe';
+import { tablerCheck, tablerCopy, tablerLink } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-preview',
@@ -11,15 +12,28 @@ import { SafeUrlPipe } from '../../../../pipes/safe-url.pipe';
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.css',
   providers: [BookmarkletPipe],
+  viewProviders: [
+    provideIcons({
+      check: tablerCheck,
+      copy: tablerCopy,
+      link: tablerLink,
+    }),
+  ],
 })
 export class PreviewComponent {
   readonly #bookmarkletPipe = inject(BookmarkletPipe);
+
+  #copiedCount = signal<number>(0);
+  readonly copied = computed(() => this.#copiedCount() > 0);
 
   readonly name = input.required<string>();
   readonly code = input.required<string>();
 
   copyToClipboard() {
     const bookmarklet = this.#bookmarkletPipe.transform(this.code());
-    copy(bookmarklet);
+    if (!copy(bookmarklet)) return;
+
+    this.#copiedCount.update((count) => count + 1);
+    setTimeout(() => this.#copiedCount.update((count) => count - 1), 1500);
   }
 }
